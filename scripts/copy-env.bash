84fb5add2e7921f571f84db06c19a12fb6bc8a24
ramity <lewisclbrown@gmail.com>
2025-03-22 23:08:48 -0500

    removed notification backend cp command

--- scripts/copy-env.bash
+++ scripts/copy-env.bash
@@ -4,4 +4,3 @@ cp -u docker/cache/.env.dist docker/cache/.env
 cp -u docker/core-backend/.env.dist docker/core-backend/.env
 cp -u docker/database/.env.dist docker/database/.env
 cp -u docker/database-viewer/.env.dist docker/database-viewer/.env
-cp -u docker/notification-backend/.env.dist docker/notification-backend/.env

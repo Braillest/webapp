74372407b45f2a835b42279bcc68e1f5570b964f
ramity <lewisclbrown@gmail.com>
2025-04-01 19:25:37 -0500

    added download_books script from automation repo + added new data dirs + their exclusions in .gitignore

---
+++ src/core-backend/python/download_books.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+book_dir = "/data/source-material/books/"
+text_dir = "/data/source-material/texts/"
+url = "https://www.gutenberg.org/browse/scores/top"
+
+response = requests.get(url)
+response.raise_for_status()
+html = response.content
+soup = BeautifulSoup(html, "html.parser")
+h2 = soup.find("h2", id="books-last1")
+ol = h2.find_next_sibling("ol")
+titles = [li.get_text() for li in ol.find_all("li")]
+links = ["https://www.gutenberg.org" + a["href"] + ".txt.utf-8" for a in ol.find_all("a")]
+books = [requests.get(link).text for link in links]
+
+for title, book in zip(titles, books):
+
+    # Save complete context of the book for reference
+    with open(book_dir + title + ".txt", "w") as file:
+        file.write(book)
+
+    # Skip gutenberg copyright and extract book text
+    text = str(book).split("***")[2]
+    with open(text_dir + title + ".txt", "w") as file:
+        file.write(text)
